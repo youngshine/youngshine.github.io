471f2a3e8fc8a0afbdece5cd738927525a2e724a
Young <lin8808@tom.com>
2019-06-21 17:38:17 +0800

    Update app.js

--- examples/81/album/app.js
+++ examples/81/album/app.js
@@ -1,11 +1,23 @@
 
 //"use strict";
 
+var Loading = {
+  template: `
+  <sui-dimmer active>
+    <sui-loader>Loading...</sui-loader>
+  </sui-dimmer>
+  `,
+}
+
 new Vue({
   el: '#app',
-  template: `
-  <div>
+  mounted() {
+    document.querySelector("#loadingPage").remove()
+  },
 
+  template: `
+  <div @click="previewImg(event)">
+    <p style="padding:10px 10px 10px 75px;color:#fff;">岁月如歌。</p>
     <div class="HolyGrail">
       <div class="HolyGrail-body">
         <nav class="HolyGrail-nav">2015<br>福州<br>宁德</nav>
@@ -116,7 +128,7 @@ new Vue({
     <br>
     <div class="HolyGrail">
       <div class="HolyGrail-body">
-        <nav class="HolyGrail-nav">1995<br>福州</nav>
+        <nav class="HolyGrail-nav">1995<br>福州<br>母校</nav>
         <main class="parent HolyGrail-content">
           <img class="child" src="./images/1995/thumb/001.jpg" />
           <img class="child" src="./images/1995/thumb/002.jpg" />
@@ -143,11 +155,44 @@ new Vue({
   </div>
   `,
   methods: {
+      previewImg(e){
+          console.log(e.target)
+          if(!e.target.src) return
+          var thumbUrl = e.target.src
+          var imgUrl = thumbUrl.replace('/thumb','');
+          var imgPath = thumbUrl.substr(0,thumbUrl.indexOf('thumb'))
+          //console.log(imgPath)
+          var arr = [];
+          arr.push(imgPath+'001.jpg')
+          arr.push(imgPath+'002.jpg')
+          arr.push(imgPath+'003.jpg')
+          arr.push(imgPath+'004.jpg')
+          arr.push(imgPath+'005.jpg')
+          arr.push(imgPath+'006.jpg')
+          arr.push(imgPath+'007.jpg')
+          arr.push(imgPath+'008.jpg')
+          arr.push(imgPath+'009.jpg')
+          arr.push(imgPath+'010.jpg')
+          arr.push(imgPath+'011.jpg')
+          arr.push(imgPath+'012.jpg')
+          arr.push(imgPath+'013.jpg')
+          arr.push(imgPath+'014.jpg')
+          arr.push(imgPath+'015.jpg')
+          arr.push(imgPath+'016.jpg')
+          arr.push(imgPath+'017.jpg')
+          arr.push(imgPath+'018.jpg')
+          console.log('current: ',imgUrl)
+          console.log('arr: ',arr) //[imgUrl]  
+          WeixinJSBridge.invoke('imagePreview', {  
+            'current' : imgUrl, 
+            'urls'    : arr  
+          })
+      }, /*
       showDialog() {
           this.$dialog({
               title: "确定删除此订单？",
               content: "删除后将从你的记录里消失，无法找回"
           });
-      }
+      } */
   }
 });
